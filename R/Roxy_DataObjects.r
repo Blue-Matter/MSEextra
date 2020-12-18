63bb88ebcf794edbbb334b6b0ed9903287b48d0e
Adrian <ar.hordyk@gmail.com>
2020-12-17 16:16:14 -0800

    first commit

---
+++ R/Roxy_DataObjects.r
@@ -0,0 +1,118 @@
+# This file is automatically built by build_package/build_OM_case_studies.R 
+# Don't edit by hand!
+# 
+
+#' @name MSEextra-OM
+#' @aliases MSEextra MSEextra-package
+#' @docType data
+#' @title Operating models and MSE output for openMSE
+#' @description A suite of operating models (class OM)
+#' @references \url{}
+#' @examples
+#' library(openMSE)
+#' library(MSEextra)
+#' myMSE <- runMSE(Shortspine_Thornyhead_BC_DFO)
+#' plot(Shortspine_Thornyhead_BC_DFO)
+NULL
+
+
+#' @rdname MSEextra-OM
+"Almaco_Jack_GOM_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Banded_Morwong_Tasmania"
+
+
+#' @rdname MSEextra-OM
+"Barred_Sand_Bass_CA_CDFW"
+
+
+#' @rdname MSEextra-OM
+"California_Halibut_CA_CDFW"
+
+
+#' @rdname MSEextra-OM
+"Canary_Rockfish_BC_DFO"
+
+
+#' @rdname MSEextra-OM
+"Gatuzo_Argentina_INIDEP"
+
+
+#' @rdname MSEextra-OM
+"Haddock_Faroe_Islands"
+
+
+#' @rdname MSEextra-OM
+"Hogfish_PR_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Jonah_Crab_LFA34_DFO"
+
+
+#' @rdname MSEextra-OM
+"Lane_Snapper_GOM_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Lesser_Amberjack_GOM_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Pacific_Hake_USCan"
+
+
+#' @rdname MSEextra-OM
+"Queen_Triggerfish_STT_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Red_Drum_GOM_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Red_Sea_Urchin_CA_CDFW"
+
+
+#' @rdname MSEextra-OM
+"Shortspine_Thornyhead_BC_DFO"
+
+
+#' @rdname MSEextra-OM
+"Snowy_Grouper_GOM_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Speckled_Hind_GOM_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Spiny_Lobster_STX_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Stones_Sheep_BC"
+
+
+#' @rdname MSEextra-OM
+"Stoplight_Parrotfish_STX_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Warty_Sea_Cucumber_CA_CDFW"
+
+
+#' @rdname MSEextra-OM
+"Yelloweye_Rockfish_BC_DFO"
+
+
+#' @rdname MSEextra-OM
+"Yellowmouth_Grouper_GOM_NOAA"
+
+
+#' @rdname MSEextra-OM
+"Yellowtail_Snapper_PR_NOAA"
+
+
---
+++ build_package/build_OM_case_studies.R
@@ -0,0 +1,113 @@
+library(openMSE)
+library(dplyr)
+library(usethis)
+
+setwd("C:/Users/Adrian/Documents/GitHub/MSEextra")
+
+pkg.dir <- getwd()
+if (!grepl('MSEextra', pkg.dir))
+  stop('Current directory must be MSEextra project')
+OM.path <- 'G:/Shared drives/BM shared/2. Resources/3_OM_case_studies'
+
+OM.dirs <- list.dirs(OM.path, recursive = FALSE)
+OM.dirs <- OM.dirs[!grepl('Z - INCOMPLETE', OM.dirs)]
+
+OMs <- basename(OM.dirs) # names of the OMs
+OMdf <- data.frame(OMs=OMs, path=OM.dirs, build=TRUE, success=TRUE, error=NA)
+
+# set build=FALSE to skip re-building (e.g. importing from SS)
+
+# ---- Build the OMs and Documentation for each OM in OMdf (where build=TRUE) ----
+for (XX in 1:nrow(OMdf)) {
+  OMname <- as.character(OMdf$OMs[XX])
+  message(OMname)
+  fp <- OMdf$path[XX]
+
+  # Build the OM and documentation
+  if (OMdf$build[XX]) {
+    unlink(file.path(fp,'build'), recursive=TRUE, force=TRUE) # delete old build files
+    setwd(fp)
+    tt <- try(source('Build.r'))
+    if (class(tt) == 'try-error') {
+      OMdf$success[XX] <- FALSE
+      OMdf$error[XX] <- tt[1]
+    }
+    setwd(pkg.dir)
+  }
+}
+
+if (any(!OMdf$success))
+  stop('Some OMs did not build successfully!')
+
+
+# ---- Copy over files to MSEextra ----
+MSEextra.path <- 'OM_case_studies'
+
+RoxygenFile <- "Roxy_DataObjects.r" # name of R script with roxygen
+file.remove(file.path(pkg.dir, 'R/', RoxygenFile)) # delete
+file.create(file.path(pkg.dir, 'R/', RoxygenFile)) # make empty file
+
+cat("# This file is automatically built by build_package/build_OM_case_studies.R \n",
+    "# Don't edit by hand!\n",
+    "# \n\n", sep="", append=TRUE,
+    file=file.path(pkg.dir, 'R/', RoxygenFile))
+
+cat("#' @name MSEextra-OM",
+    "\n#' @aliases MSEextra MSEextra-package",
+    "\n#' @docType data",
+    "\n#' @title Operating models and MSE output for openMSE",
+    "\n#' @description A suite of operating models (class OM)",
+    "\n#' @references \\url{}",
+    "\n#' @examples",
+    "\n#' library(openMSE)",
+    "\n#' library(MSEextra)",
+    "\n#' myMSE <- runMSE(Shortspine_Thornyhead_BC_DFO)",
+    "\n#' plot(Shortspine_Thornyhead_BC_DFO)",
+    '\nNULL',
+    "\n\n\n",
+    sep="", append=TRUE,
+    file=file.path(pkg.dir, 'R/', RoxygenFile))
+
+
+OMdf <- OMdf %>% dplyr::filter(success==TRUE)
+for (XX in 1:nrow(OMdf)) {
+    OMname <- as.character(OMdf$OMs[XX])
+    message(OMname)
+
+    src.path <- file.path(OM.path, OMname)
+    out.path <- file.path(MSEextra.path,OMname)
+
+    # create directory
+    if (!dir.exists(out.path)) dir.create(out.path)
+
+    # copy docs folder
+    if (dir.exists(file.path(src.path, 'docs'))) {
+      if (!dir.exists(file.path(out.path, 'docs'))) dir.create(file.path(out.path, 'docs'))
+      file.copy(file.path(src.path, 'docs'), out.path,
+                overwrite = TRUE, recursive = TRUE)
+    }
+
+    # copy OM.rdata
+    if (file.exists(file.path(src.path, "OM.rdata"))) {
+      file.copy(file.path(src.path, "OM.rdata"), file.path(out.path, "OM.rdata"),
+                overwrite = TRUE)
+
+      # add to MSEextra data
+      OM <- readRDS(file.path(out.path, "OM.rdata"))
+      assign(OMname, OM)
+      do.call("use_data", list(as.name(OMname), overwrite = TRUE))
+
+      # Write roxygen
+      cat("#' @rdname MSEextra-OM", '\n"', OMname, '"\n\n\n', sep="", append=TRUE,
+          file=file.path(pkg.dir, 'R/', RoxygenFile))
+
+    }
+
+    # copy html
+    if (file.exists(file.path(src.path, "OM.html")))
+      file.copy(file.path(src.path, "OM.html"), file.path(out.path, "OM.html"),
+                overwrite = TRUE)
+
+}
+
+
