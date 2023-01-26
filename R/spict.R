bc502088eb7268433cdc1a574c41f9e298e04b88
Quang C. Huynh <quang@bluematterscience.com>
2023-01-25 18:22:02 -0800

    Update spict

--- R/spict.R
+++ R/spict.R
@@ -10,21 +10,22 @@
 #' @param x An index for the objects in \code{Data} when running in \link[DLMtool]{runMSE}.
 #' Otherwise, equals to 1 when running an assessment interactively.
 #' @param Data An object of class Data.
-#' @param start Optional list of starting values. See details.
+#' @param inp_args A named of list of additional variables for the \code{inp} list to pass to \link[spict]{check.inp}. Core function for configuring spict.
+#' @param start Optional list of starting values. See details. Overrides \code{inp_args}.
 #' @param fix_dep Logical, whether to fix the initial depletion (ratio of biomass to carrying capacity in the
 #' first year of the model). If \code{TRUE}, uses the value in \code{start}, otherwise equal to 1
-#' (assumes unfished conditions).
+#' (assumes unfished conditions). Overrides \code{inp_args}.
 #' @param fix_n Logical, whether to fix the exponent of the production function. If \code{TRUE},
 #' uses the value in \code{start}, otherwise equal to \code{n = 2}, where the biomass at MSY
-#' is half of carrying capacity.
+#' is half of carrying capacity. Overrides \code{inp_args}.
 #' @param fix_sigma Logical, whether the standard deviation of the index is fixed. If \code{TRUE},
-#' sigma is fixed to value provided in \code{start} (if provided), otherwise, value based on \code{Data@@CV_Ind}.
+#' sigma is fixed to value provided in \code{start} (if provided), otherwise, value based on \code{Data@@CV_Ind}. Overrides \code{inp_args}.
 #' @param fix_omega Logical, whether the standard deviation of the catch is fixed. If \code{TRUE},
-#' omega is fixed to value provided in \code{start} (if provided), otherwise, value based on \code{Data@@CV_Cat}.
+#' omega is fixed to value provided in \code{start} (if provided), otherwise, value based on \code{Data@@CV_Cat}. Overrides \code{inp_args}.
 #' @param fix_alpha Logical, whether the ratio of index and biomass standard deviations is fixed. If \code{TRUE},
-#' alpha is fixed to value provided in \code{start} (if provided), otherwise, equal to 1.
+#' alpha is fixed to value provided in \code{start} (if provided), otherwise, equal to 1. Overrides \code{inp_args}.
 #' @param fix_beta Logical, whether the ratio of catch and F standard deviations is fixed. If \code{TRUE},
-#' tau is fixed to value provided in \code{start} (if provided), otherwise, equal to 1.
+#' tau is fixed to value provided in \code{start} (if provided), otherwise, equal to 1. Overrides \code{inp_args}.
 #' @param n_seas Integer, the number of sub-annual time steps in the model, i.e. the inverse of the Euler time step. Defaults to 4 (seasonal time steps).
 #' @param ... Additional arguments (not currently used).
 #' @details
@@ -33,7 +34,7 @@
 #' @return An object of \code{\linkS4class{Assessment}} containing assessment output.
 #' @note
 #' This is a wrapper function intended for a DLMtool \linkS4class{Data} object. The \code{spict} package can be
-#' downloaded from Github with \code{devtools::install_github("mawp/spict/spict")}.
+#' downloaded from Github with \code{devtools::install_github("DTUAqua/spict/spict")}.
 #'
 #' The full spict model also accommodates time-varying reference points and seasonal data among other
 #' things, but these features are currently not used here. Additional diagnostics and plotting functions are also available in the spict package.
@@ -46,7 +47,9 @@
 #' \item \code{spict}: Cat, Ind
 #' }
 #' @section Optional Data:
-#' \code{spict}: CV_Cat, CV_Ind
+#' \itemize{
+#' \item \code{spict}: CV_Cat, CV_Ind
+#' }
 #' @examples
 #' \donttest{
 #' library(openMSE)
@@ -56,7 +59,7 @@
 #' plot(res)
 #' summary(res)
 #'
-#' ## Use additional functions spict functions
+#' ## Use additional spict-package functions
 #' spict_output <- res@@info
 #' library(spict)
 #' summary(spict_output)
@@ -69,7 +72,7 @@
 #' @importMethodsFrom SAMtool summary plot
 #' @seealso \link[SAMtool]{SP_production} \link[SAMtool]{plot.Assessment} \link[SAMtool]{summary.Assessment} \link[SAMtool]{make_MP}
 #' @export spict SPiCT
-spict <- SPiCT <- function(x = 1, Data, start = NULL, fix_dep = TRUE, fix_n = TRUE, fix_sigma = FALSE,
+spict <- SPiCT <- function(x = 1, Data, inp_args = list(), start = NULL, fix_dep = TRUE, fix_n = TRUE, fix_sigma = FALSE,
                            fix_omega = FALSE, fix_alpha = TRUE, fix_beta = TRUE, n_seas = 4L, ...) {
   check_dependencies("spict", "spict")
   dependencies = "Data@Cat, Data@Ind"
@@ -88,8 +91,13 @@ spict <- SPiCT <- function(x = 1, Data, start = NULL, fix_dep = TRUE, fix_n = TR
   I_hist[I_hist < 0] <- NA
   ny <- length(C_hist)
 
-  inp <- spict::check.inp(list(obsC = C_hist, timeC = Year, obsI = I_hist[!is.na(I_hist)], timeI = Year[!is.na(I_hist)],
-                               dteuler = 1/n_seas))
+  inp_args[["obsC"]] <- C_hist
+  inp_args[["timeC"]] <- Year
+  inp_args[["obsI"]] <- I_hist[!is.na(I_hist)]
+  inp_args[["timeI"]] <- Year[!is.na(I_hist)]
+  inp_args[["dteuler"]] <- 1/n_seas
+
+  inp <- spict::check.inp(inp_args)
   inp$getReportCovariance <- inp$getJointPrecision <- FALSE
 
   if(!is.null(start$MSY) && is.numeric(start$MSY)) inp$ini$logm <- log(start$MSY[1])
@@ -105,8 +113,6 @@ spict <- SPiCT <- function(x = 1, Data, start = NULL, fix_dep = TRUE, fix_n = TR
   if(fix_n) {
     val <- ifelse(!is.null(start$n), start$n[1], 2)
     inp$priors$logn <- c(log(val), 1e-2, 1)
-  } else {
-    inp$priors$logn[3] <- 0
   }
   if(fix_sigma) {
     val <- ifelse(!is.null(start$sigma), start$sigma[1], sdconv(1, Data@CV_Ind[x]))
@@ -141,28 +147,33 @@ spict <- SPiCT <- function(x = 1, Data, start = NULL, fix_dep = TRUE, fix_n = TR
   Yearplusone <- c(Year, max(Year) + 1)
   plus_one_ind <- c(season_1_ind, ny * n_seas + 1)
 
-  K <- exp(res$par.fixed[names(res$par.fixed) == "logK"])
-  B1 <- exp(SD$par.random[names(SD$par.random)=='logB'][season_1_ind])
-  B2 <- exp(SD$par.random[names(SD$par.random)=='logB'][plus_one_ind])
-  Ipred <- exp(res$par.fixed[names(res$par.fixed) == "logq"]) * B1
+  K <- exp(res$par.fixed["logK"])
+  Ipred <- exp(res$par.fixed["logq"]) * spict::get.par("logB", res, exp = TRUE)[season_1_ind, "est"]
 
-  FMort <- exp(SD$par.random[names(SD$par.random)=='logF'][season_1_ind])
-
-  Assessment <- new("Assessment", Model = "spict", Name = Data@Name, conv = is.null(res$sderr) && SD$pdHess,
-                    FMSY = report$Fmsy, MSY = report$MSY, BMSY = report$Bmsy, VBMSY = report$Bmsy, SSBMSY = report$Bmsy,
+  B <- spict::get.par("logB", res, exp = TRUE)[plus_one_ind, "est"]
+  B_BMSY <- spict::get.par("logBBmsy", res, exp = TRUE)[plus_one_ind, "est"]
+  B_B0 <- B/K
+  Assessment <- new("Assessment",
+                    Model = "spict",
+                    Name = Data@Name,
+                    conv = is.null(res$sderr) && SD$pdHess,
+                    FMSY = report$Fmsy,
+                    MSY = report$MSY,
+                    BMSY = report$Bmsy,  VBMSY = report$Bmsy, SSBMSY = report$Bmsy,
                     B0 = K, VB0 = K, SSB0 = K,
-                    FMort = structure(FMort, names = Year),
-                    F_FMSY = structure(FMort/report$Fmsy, names = Year),
-                    B = structure(B2, names = Yearplusone),
-                    B_BMSY = structure(B2/report$Bmsy, names = Yearplusone),
-                    B_B0 = structure(B2/K, names = Yearplusone),
-                    VB = structure(B2, names = Yearplusone),
-                    VB_VBMSY = structure(B2/report$Bmsy, names = Yearplusone),
-                    VB_VB0 = structure(B2/K, names = Yearplusone),
-                    SSB = structure(B2, names = Yearplusone),
-                    SSB_SSBMSY = structure(B2/report$Bmsy, names = Yearplusone),
-                    SSB_SSB0 = structure(B2/K, names = Yearplusone),
-                    Obs_Catch = structure(C_hist, names = Year), Obs_Index = structure(I_hist, names = Year),
+                    FMort = spict::get.par("logF", res, exp = TRUE)[season_1_ind, "est"],
+                    F_FMSY = spict::get.par("logFFmsy", res, exp = TRUE)[season_1_ind, "est"],
+                    B = B,
+                    B_BMSY = B_BMSY,
+                    B_B0 = B_B0,
+                    VB = B,
+                    VB_VBMSY = B_BMSY,
+                    VB_VB0 = B_B0,
+                    SSB = B,
+                    SSB_SSBMSY = B_BMSY,
+                    SSB_SSB0 = B_B0,
+                    Obs_Catch = structure(C_hist, names = Year),
+                    Obs_Index = structure(I_hist, names = Year),
                     Index = structure(Ipred, names = Year),
                     NLL = ifelse(is.list(opt), opt$objective, NA),
                     info = res, obj = obj, opt = opt, TMB_report = report,
@@ -175,21 +186,22 @@ spict <- SPiCT <- function(x = 1, Data, start = NULL, fix_dep = TRUE, fix_n = TR
       Assessment@FMSY <- SD$value["Fmsyd"]
       Assessment@BMSY <- SD$value["Bmsyd"]
 
-      Assessment@F_FMSY <- Assessment@FMort/Assessment@F_FMSY
-      Assessment@B_BMSY <- Assessment@VB_VBMSY <- Assessment@SSB_SSBMSY <- Assessment@B/Assessment@BMSY
+      #Assessment@F_FMSY <- Assessment@FMort/Assessment@F_FMSY
+      #Assessment@B_BMSY <- Assessment@VB_VBMSY <- Assessment@SSB_SSBMSY <- Assessment@B/Assessment@BMSY
     }
 
     Assessment@SE_FMSY <- SD$sd[names(SD$value) == "Fmsy"]
     Assessment@SE_MSY <- SD$sd[names(SD$value) == "MSY"]
 
-    # Assessment@SE_F_FMSY_final <- delta_log(SD$value[names(SD$value) == "logFFmsy"][ny*n_seas-n_seas+1],
-                                            # SD$sd[names(SD$value) == "logFFmsy"][ny*n_seas-n_seas+1])
-    # Assessment@SE_B_BMSY_final <- Assessment@SE_VB_VBMSY_final <-
-      # delta_log(SD$value[names(SD$value) == "logBBmsy"][ny*n_seas+1], SD$sd[names(SD$value) == "logBBmsy"][ny*n_seas+1])
+    #delta_log <- function(mu, std) exp(mu) * std
+    #Assessment@SE_F_FMSY <- delta_log(SD$value[names(SD$value) == "logFFmsy"][ny*n_seas-n_seas+1],
+    #                                  SD$sd[names(SD$value) == "logFFmsy"][ny*n_seas-n_seas+1])
+    #Assessment@SE_B_BMSY <- Assessment@SE_VB_VBMSY <-
+    #  delta_log(SD$value[names(SD$value) == "logBBmsy"][ny*n_seas+1], SD$sd[names(SD$value) == "logBBmsy"][ny*n_seas+1])
   }
   if(is.null(res$sderr)) {
     Assessment@SD <- SD
-    Cpred <- spict::get.par("logCpred", res, exp = TRUE)[1:ny, 2] # NULL if errored
+    Cpred <- spict::get.par("logCpred", res, exp = TRUE)[1:ny, "est"] # NULL if errored
     Assessment@Catch <- structure(Cpred, names = Year)
   }
 
@@ -197,4 +209,3 @@ spict <- SPiCT <- function(x = 1, Data, start = NULL, fix_dep = TRUE, fix_n = TR
 }
 class(spict) <- class(SPiCT) <- "Assess"
 
-delta_log <- function(mu, std) exp(mu) * std
