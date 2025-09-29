5d34b3dd35a50801a517d17362d34a8b676010c9
Adrian Hordyk <ar.hordyk@gmail.com>
2025-09-29 14:02:31 -0700

    Update check_dependencies.R

--- R/check_dependencies.R
+++ R/check_dependencies.R
@@ -3,7 +3,7 @@ check_dependencies <- function(func_name, pkg) {
   need_pkg <- !requireNamespace(pkg, quietly = TRUE)
 
   if(need_pkg) {
-    if(pkg == "spict") pkg_repo <- "DTUAqua/spict"
+    if(pkg == "spict") pkg_repo <- "mawp/spict/spict"
     if(pkg == "stockassessment") pkg_repo <- "fishfollower/SAM/stockassessment"
   }
 
@@ -11,7 +11,7 @@ check_dependencies <- function(func_name, pkg) {
     msg_out <- paste0("The following package(s) are needed to run ", func_name, " function:")
     if(need_SAMtool) msg_out <- paste0(msg_out, '\nMSEtool - download from CRAN via install.packages("SAMtool")')
 
-    if(need_pkg) msg_out <- paste0(msg_out, "\n", pkg, ' - download from Github via devtools::install_github("', pkg_repo, '")')
+    if(need_pkg) msg_out <- paste0(msg_out, "\n", pkg, ' - download from Github via pak::pkg_install("', pkg_repo, '")')
 
     stop(msg_out, call. = FALSE)
   }
